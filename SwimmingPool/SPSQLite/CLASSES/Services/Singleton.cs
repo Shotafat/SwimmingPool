24781a54e4b210a4633479becb1995096af60f2d
DataKhukhua <khukhuadata@gmail.com>
2019-01-10 19:29:12 +0400

    pp

---
+++ SwimmingPool/SPSQLite/CLASSES/Services/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPSQLite.CLASSES.Services
+{
+   public  class ServiceInstances
+    {
+
+
+
+        public ISubscriberServices GetSubscriberService() {
+
+            return new SubscriberServices();
+        }
+
+
+
+    }
+}
